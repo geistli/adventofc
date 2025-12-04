9c330b70b16ba4ec0392d3c80e3ef208e85c1922
Benjamin <benjamin@spaceforce.li>
2025-12-04 10:20:00 +0100

    Implement Day 2.1 challenge with input parsing and repeating number check

---
+++ src/day2.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 512
+
+int is_repeating(const char *str) {
+    int len = strlen(str);
+    for (int nparts = 2; nparts < 3; nparts++) {
+        if (strlen(str) % nparts != 0) {
+            return 0;
+        }
+
+        int partlength = len / nparts;
+
+        for (int position = 0; position < partlength; position++) {
+            if (str[position] != str[position + partlength]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    FILE *file;
+    char line[MAX_LINE];
+    long c = 0;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file %s\n", argv[1]);
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        line[strcspn(line, "\n")] = 0;
+        char *token = strtok(line, ",");
+        while (token != NULL) {
+            long start, end;
+            sscanf(token, "%ld-%ld", &start, &end);
+
+            for (long i = start; i < end + 1; i++) {
+                char num_str[20];
+                sprintf(num_str, "%ld", i);
+                if (is_repeating(num_str)) {
+                    c += i;
+                
+                    if (c < 0){
+                        printf("token: %s\n", token);
+                        printf("st en: %ld %ld\n", start, end);
+                        printf("cur n: %ld\n", i);
+                        printf("c: %ld\n", c);
+                    }}
+            }
+
+            token = strtok(NULL, ",");
+        }
+    }
+    printf("result: %ld\n", c);
+
+    fclose(file);
+    return 0;
+}
